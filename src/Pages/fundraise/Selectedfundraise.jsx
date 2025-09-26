4a604b14b196d9ecac9b9c8b9330b6ed12631657
ronakvarshney <ronakvarshney7100@gmail.com>
2025-09-26 13:54:28 +0530

    fix

--- src/Pages/fundraise/Selectedfundraise.jsx
+++ src/Pages/fundraise/Selectedfundraise.jsx
@@ -8,7 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog";
+} from "@/components/ui/dialog.tsx";
 
 const Selectedfundraise = () => {
   const { id } = useParams();
